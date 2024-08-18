7cf074b5c5462f10769f7af197b976f5bb14486e
ark1500j <arkwashington52@gmail.com>
2024-08-18 15:52:18 +0000

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -7,14 +7,14 @@ export function middleware(request:Request){
     const origin =request.headers.get('origin') 
     console.log(origin && !allowedOrigins.includes(origin))
 //   add to condition if you want to block rest client or postman ||!origin 
-    if(origin && !allowedOrigins.includes(origin)) return new NextResponse(null,{ 
-        status:400, 
-        statusText:"bad request", 
-        headers:{ 
-            'Content-Type':'text/plain' 
-        } 
-    }) 
-    // console.log(origin) 
+    // if(origin && !allowedOrigins.includes(origin)) return new NextResponse(null,{ 
+    //     status:400, 
+    //     statusText:"bad request", 
+    //     headers:{ 
+    //         'Content-Type':'text/plain' 
+    //     } 
+    // }) 
+    // // console.log(origin) 
     // console.log("Middleware") 
     // console.log(request.method) 
     // console.log(request.url) 
